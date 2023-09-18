41c43713856ff23f5524d61c3098ca6e55672cb8
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-18 18:14:31 +0700

    create(file): implements enum modularization

--- App/app.dart
+++ App/app.dart
@@ -1,35 +1,5 @@
 import 'Class/Spacecraft.dart';
-
-enum PlanetType {
-  terrestrial,
-  gas,
-  ice
-}
-
-/// Enum that enumerates the different planets in our solar system
-/// and some of their properties.
-enum Planet {
-  earth(planetType: PlanetType.terrestrial, moons: 1, hasRings: false),
-  mercury(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
-  venus(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
-  uranus(planetType: PlanetType.ice, moons: 27, hasRings: true),
-  neptune(planetType: PlanetType.ice, moons: 14, hasRings: true);
-
-  /// A constant generating constructor
-  const Planet({
-    required this.planetType,
-    required this.moons,
-    required this.hasRings
-  });
-
-  /// All instance variables are final
-  final PlanetType planetType;
-  final int moons;
-  final bool hasRings;
-
-  /// Enhanced enums support getters and other methods
-  bool get isGiant => planetType == PlanetType.gas || planetType == PlanetType.ice;
-}
+import 'Enum/Planet.dart';
 
 void main() {
   var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
