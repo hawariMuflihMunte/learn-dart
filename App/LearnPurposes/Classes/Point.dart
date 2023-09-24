08451b3424515eccc82b57f976161792531c279f
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-24 12:03:41 +0700

    refactor(dart:Point): addFunc([move, show])

--- App/LearnPurposes/Classes/Point.dart
+++ App/LearnPurposes/Classes/Point.dart
@@ -1,10 +1,22 @@
 class Point {
   int x = 0;
   int y = 0;
+
+  void move(int x1, int y1) {
+    x = x1;
+    y = y1;
+  }
+
+  void show() {
+    print('Point($x, $y)');
+  }
 }
 
 void main() {
   var p1 = Point()
     ..x = 10
     ..y = 20;
+
+  p1.move(100, 200);
+  p1.show();
 }
