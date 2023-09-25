1b8373fcb71fb2dd033d317c360db0921ba0b2de
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-25 21:49:36 +0700

    `create(file:dart)`: main.dart - Implements Inheritances and `is` operator

---
+++ App/LearnPurposes/ClassesAdv/main.dart
@@ -0,0 +1,44 @@
+
+class BankAccount {
+  double _balance = 0;
+
+  double get balance => _balance;
+
+  deposit(double amount) {
+    _balance += amount;
+  }
+
+  bool withdraw(double amount) {
+    if (amount <= _balance) {
+      _balance -= amount;
+      return true;
+    }
+    return true;
+  }
+}
+
+class SavingAccount extends BankAccount {
+  double _interestRate = 0;
+
+  double get interestRate => _interestRate;
+
+  set interestRate(double value) {
+    if (value > 0) {
+      _interestRate = value;
+    }
+  }
+
+  addInterest() {
+    double interest = _balance * _interestRate;
+    this._balance += interest;
+  }
+}
+
+void main() {
+  var account = SavingAccount();
+  account.deposit(1000);
+  account.withdraw(200);
+
+  print(account is SavingAccount);
+  print(account is BankAccount);
+}
