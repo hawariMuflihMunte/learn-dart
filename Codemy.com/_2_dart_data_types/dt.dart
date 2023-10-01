da35c4d6dcb9a0104cf66245c058fbc5773bd39f
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-01 16:38:15 +0700

    Codemy.com(Data Types)

---
+++ Codemy.com/_2_dart_data_types/dt.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Data Types
+  // Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+  // Strings
+  String firstName = 'John';
+  print('String ${firstName}');
+
+  // Integers
+  int myInt = 41;
+  print('Integer: ${myInt}');
+
+  // Doubles
+  double myDouble = 3.19;
+  print('Double: ${myDouble}');
+
+  // Booleans
+  bool myBool = true;
+  print('Boolean: ${myBool}');
+
+  // Dynamic
+  dynamic fullName = 'John Wick';
+  print('Dynamic: ${fullName}');
+}
